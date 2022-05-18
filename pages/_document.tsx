5cb441d4fe6115fa9ea5cdb014a70ae9107dd62d
Teerut26 <teerutsr@gmail.com>
2022-05-18 11:58:24 +0700

    add title

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,7 @@ export default function Document() {
     return (
         <Html className="duration-500">
             <Head>
+                <title>Teerut Portfolio</title>
                 <link
                     href="https://fonts.googleapis.com/css2?family=Pacifico&family=Prompt:wght@100;200;300;400;500;600;700&display=swap"
                     rel="stylesheet"
